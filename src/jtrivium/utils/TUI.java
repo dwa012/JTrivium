90d2ffb66f8ae28eb39129637464bd0d8aafaa61
Daniel Ward <dwa012@gmail.com>
2011-11-15 12:49:39 -0600

    restructured the packages, to make more logical sense

--- src/jtrivium/utils/TUI.java
+++ src/jtrivium/utils/TUI.java
@@ -15,8 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package jtrivium.cipher;
+package jtrivium.utils;
 
+import jtrivium.cipher.JTrivium;
 import jtrivium.utils.Utility;
 
 /**
